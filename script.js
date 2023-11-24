019df7e520ca0b5d776b41f01833b4c34aa376fc
codeldu <huseynagha.hagverdiyev@code.edu.az>
2023-11-24 17:54:23 +0400

    initial

---
+++ script.js
@@ -0,0 +1,54 @@
+// // 'use strict'
+
+// const restoran = {
+//     name : 'Yashil ada',
+//     // newLocation : '3cu dalan, Baki',
+//     categories : ['Milli', 'Gurcu', 'Italyan', 'Vegetarian'],
+//     starterMenu : ['Coban salati', 'Manqal salati', 'Suzme', 'Sezar', 'Turshu'],
+//     mainMenu : ['Kabab', 'Pizza', 'Xengel', 'Dushbere', 'Pasta'],
+//     order : function(starterInd, mainIndex){
+//         return [ this.starterMenu[starterInd], this.mainMenu[mainIndex] ]
+//     }
+// }
+
+
+// let { categories :a   = [], newLocation : b = "Online"} = restoran;
+
+// console.log(a);
+// console.log(b);
+
+
+// let a= 1;
+// let b =2;
+
+// let myObj = {a: 34, b: 45};
+
+// ({a,b} = myObj)
+
+
+
+// console.log(a,b);
+
+// let [first, ,second] = restoran.categories;
+
+// console.log(first , second);
+
+// [second,first] = [first,second];
+
+
+// console.log(first , second);
+
+
+// let [ xolodniy, qoryachi ] = restoran.order(3, 2);
+
+
+// console.log(xolodniy);
+// console.log(qoryachi);
+
+
+// let arr = [1,2,3,6,7]
+
+// let [a,b,c,d] = "65";
+
+// console.log(c);
+
